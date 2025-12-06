af7de3a93fad232e47edef7efbe633baeed5e7d8
SafTheLamb <safthelamb@gmail.com>
2025-12-06 14:15:44 -0800

    Adjust fuel values

--- prototypes/fluid.lua
+++ prototypes/fluid.lua
@@ -7,6 +7,7 @@ data:extend({
 		order = "a[fluid]-b[oil]-b[petroleum-gas]-b[butane]",
 		default_temperature = 25,
 		gas_temperature = 25,
+		fuel_value = "900kJ", -- Balanced relative to fuel values provided by Gas Boiler
 		base_color = {0.4, 0.2, 0.4},
 		flow_color = {0.9, 0.9, 0.9}
 	},
@@ -31,7 +32,7 @@ data:extend({
 		gas_temperature = 15,
 		max_temperature = 535,
 		heat_capacity = "0.22kJ",
-		fuel_value = "5kJ",
+		fuel_value = "300kJ", -- Balanced relative to fuel values provided by Gas Boiler
 		auto_barrel = false,
 		base_color = {0.5, 0.5, 1},
 		flow_color = {1, 1, 1}
@@ -48,7 +49,7 @@ if mods["space-age"] then
 			order = "b[new-fluid]-e[aquilo]-c[hydrazine]",
 			default_temperature = 25,
 			heat_capacity = "0.44kJ",
-			fuel_value = "10kJ",
+			fuel_value = "2.4MJ",
 			base_color = {0.25, 0.25, 1},
 			flow_color = {0.75, 0.75, 1}
 		}
